faa87de342d7cffbbd460a1f274dd6670efb42ae
beatrizfig00 <beatriz.rfigueiredo@ufpe.br>
2024-07-16 20:39:41 -0300

    Organização

--- src/main/java/PotionsEmporiumFachada/Main.java
+++ src/main/java/PotionsEmporiumFachada/Main.java
@@ -1,7 +1,10 @@
 package PotionsEmporiumFachada;
 
+import javafx.application.Application;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Application.launch(PotionsEmporiumApplication.class, args);
     }
 }
