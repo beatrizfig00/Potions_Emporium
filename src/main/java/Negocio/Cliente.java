4f56bb335d5b01c9a1c12f65a1ce14d72803376f
Wilber742 <149113565+Wilber742@users.noreply.github.com>
2024-09-05 16:33:13 -0300

    Commit Cliente

---
+++ src/main/java/Negocio/Cliente.java
@@ -0,0 +1,62 @@
+package Negocio;
+
+import java.util.List;
+
+public class Cliente {
+    private int id;
+    private String nome;
+    private String coruja;
+    private String flooPowder;
+    private List<Pedido> historicoCompras;
+
+    public Cliente(int id, String nome, String coruja, String flooPowder, List<Pedido> historicoCompras) {
+        this.id = id;
+        this.nome = nome;
+        this.coruja = coruja;
+        this.flooPowder = flooPowder;
+        this.historicoCompras = historicoCompras;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCoruja() {
+        return coruja;
+    }
+
+    public void setCoruja(String coruja) {
+        this.coruja = coruja;
+    }
+
+    public String getFlooPowder() {
+        return flooPowder;
+    }
+
+    public void setFlooPowder(String flooPowder) {
+        this.flooPowder = flooPowder;
+    }
+
+    public List<Pedido> getHistoricoCompras() {
+        return historicoCompras;
+    }
+
+    public void setHistoricoCompras(List<Pedido> historicoCompras) {
+        this.historicoCompras = historicoCompras;
+    }
+
+
+    public Endereco getEndereco() {
+     return new Endereco();    }
+}
