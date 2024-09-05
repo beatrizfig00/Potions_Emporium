ec7f9cd614691a8a996bf1163594223394e84715
Nicole Oliveira <nicoleolsbcc@gmail.com>
2024-09-05 16:44:14 -0300

    Commit classe Endereco

---
+++ src/main/java/Negocio/Endereco.java
@@ -0,0 +1,42 @@
+package Negocio;
+
+public class Endereco {
+    private String rua;
+    private String cidade;
+    private int numero;
+
+    public Endereco(String rua, String cidade, int numero) {
+        this.rua = rua;
+        this.cidade = cidade;
+        this.numero = numero;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public String toString() {
+        return "Endereço: " + rua + ", " + numero + " - " + cidade;
+    }
+}
