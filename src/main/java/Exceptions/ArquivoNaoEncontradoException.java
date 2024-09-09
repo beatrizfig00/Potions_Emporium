9cf3965442ff0df0dfd15bd8b9aad1af9c6ff958
Wilber742 <149113565+Wilber742@users.noreply.github.com>
2024-09-09 02:13:55 -0300

    Update ArquivoNaoEncontradoException

--- src/main/java/Exceptions/ArquivoNaoEncontradoException.java
+++ src/main/java/Exceptions/ArquivoNaoEncontradoException.java
@@ -1,5 +1,7 @@
 package Exceptions;
 
+import java.io.IOException;
+
 public class ArquivoNaoEncontradoException extends IOException {
     public ArquivoNaoEncontradoException(String mensagem) {
         super(mensagem);
