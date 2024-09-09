e15a77b7b2d70010b0a628daa7c416fc477afc02
Wilber742 <149113565+Wilber742@users.noreply.github.com>
2024-09-09 02:20:00 -0300

    Delete src/main/java/Exceptions/AutenticacaoInvalidaException.java

--- src/main/java/Exceptions/AutenticacaoInvalidaException.java
+++
@@ -1,7 +0,0 @@
-package Exceptions;
-
-public class AutenticacaoInvalidaException extends Exception {
-    public AutenticacaoInvalidaException(String mensagem) {
-        super(mensagem);
-    }
-}
